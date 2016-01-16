9a80dfcbe1e2ddaa2f49466f81ead33537ba1397
hooman naghiee <RTLer@users.noreply.github.com>
2016-01-16 13:14:52 +0330

    fix pluck

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -556,9 +556,9 @@ class Builder extends BaseBuilder {
 
         // MongoDB returns the _id field even if you did not ask for it, so we need to
         // remove this from the result.
-        if (array_key_exists('_id', $result))
+        if (array_key_exists('_id', $results))
         {
-            unset($result['_id']);
+            unset($results['_id']);
         }
 
         return Arr::pluck(
